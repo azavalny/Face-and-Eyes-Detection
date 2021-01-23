ceb04fea0bfc46b5679759261d7223771e422b9b
Alex Zavalny <57161600+thelazyaz@users.noreply.github.com>
2021-01-23 12:50:05 -0500

    Fixed Demo

--- FaceDetection/src/WebCamFacialDetection/FacialDetectionDemo.java
+++ FaceDetection/src/WebCamFacialDetection/FacialDetectionDemo.java
@@ -7,7 +7,7 @@ public class FacialDetectionDemo {
 	public static void main(String[] args) {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		FacialDetector test = new FacialDetector("C:\\Users\\Name\\Desktop\\FaceDetection\\cascade files\\haarcascade_frontalface_alt.xml", 
-												"C:\\Users\\Desktop\\FaceDetection\\cascade fileshaarcascade_eye_tree_eyeglasses.xml");
+												"C:\\Users\\Name\\Desktop\\FaceDetection\\cascade fileshaarcascade_eye_tree_eyeglasses.xml");
 		test.runFacialDetection();
 	}
 }
